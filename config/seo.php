ff35ba7dada7147c2d173e94ae7952bc3bfd7ed2
lodebossuyt <lodebossuyt@hotmail.com>
2026-01-04 10:58:26 +0100

    setup

--- config/seo.php
+++ config/seo.php
@@ -2,5 +2,17 @@
 
 // config for Lodeb/SEO
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default SEO Settings
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default SEO settings for your application.
+    | These settings will be used unless overridden on a per-page basis.
+    |
+    */
 
+    'default_title' => 'My Awesome Website',
+    'default_description' => 'This is the best website ever built with Laravel SEO package.',
+    'default_keywords' => ['laravel', 'seo', 'package', 'awesome'],
 ];
--- src/SEOServiceProvider.php
+++ src/SEOServiceProvider.php
@@ -4,22 +4,25 @@ namespace Lodeb\SEO;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Lodeb\SEO\Commands\SEOCommand;
 
 class SEOServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-seo')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel_seo_table')
-            ->hasCommand(SEOCommand::class);
+            ->hasConfigFile();
+            // ->hasViews()
+            // ->hasMigration('create_laravel_seo_table');
+            // ->hasCommand(SEOCommand::class);
+    }
+
+    public function boot(): void
+    {
+        parent::boot();
+        
+        $this->app->singleton(SEO::class, function ($app) {
+            return new SEO();
+        });
     }
 }
